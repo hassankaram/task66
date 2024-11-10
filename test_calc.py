6c82d731f8d9b572616933ccd7564d6ac631552f
hassankaram <hassankaram898@gmail.com>
2024-11-10 16:44:43 +0200

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -3,16 +3,16 @@ import unittest
 class TestAddNumbers(unittest.TestCase):
     
     def test_add_positive_numbers(self):
-        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add(2, 3), 5)
         
     def test_add_negative_numbers(self):
-        self.assertEqual(add_numbers(-2, -3), -5)
+        self.assertEqual(add(-2, -3), -5)
         
     def test_add_mixed_numbers(self):
-        self.assertEqual(add_numbers(2, -3), -1)
+        self.assertEqual(add(2, -3), -1)
         
     def test_add_zero(self):
-        self.assertEqual(add_numbers(0, 5), 5)
+        self.assertEqual(add(0, 5), 5)
 
 if __name__ == '_main_':
     unittest.main()
